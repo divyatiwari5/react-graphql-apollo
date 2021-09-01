24770a6bcfd067138692a34edbf4171710e88ee4
Divya Tiwari <me@divyatiwari.me>
2021-09-02 03:56:11 +0530

    Implemented Pagination; Left TODO

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -6,18 +6,28 @@ import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import Search from "../../Components/Search";
 import CharacterCard from "./CharacterCard";
+import Pagination from '@material-ui/lab/Pagination';
+
 
 const useStyles = makeStyles((theme) => ({
     gridContainer: {
         margin: '60px 150px'
+    },
+    pagination: {
+        display: 'flex',
+        justifyContent: 'center',
+        marginBottom: 20
     }
 }));
 
-export const GET_CHARACTERS = (searchString: any) => gql`
+export const GET_CHARACTERS = (page = 1, searchString: any) => gql`
   query {
-      characters(page: 1, filter: { name: "${searchString}" }) {
+      characters(page: ${page}, filter: { name: "${searchString}" }) {
     info {
-      count
+      count,
+      pages,
+      next,
+      prev
     }
     results {
         name
@@ -35,20 +45,23 @@ function HomePage(props: any) {
     const searchedString = query.get("q");
 
     const classes = useStyles();
-
-    const [ loadCharacters, {loading: loadingAllChars, error: AllCharsError, data: allChars} ]= useLazyQuery(GET_CHARACTERS(""));
-
+    const [page, setPage] = useState(1);
     const [results, setResults] = useState([])
 
-    const [ searchCharacters, { loading, error, data }] = useLazyQuery(GET_CHARACTERS(searchedString))
+    const [ loadCharacters, {loading: loadingAllChars, error: AllCharsError, data: allChars} ]= useLazyQuery(GET_CHARACTERS(page, ""));
+
+    const [pageInfo, setPageInfo] = useState({count: 0, pages: 0, next: 0, prev: 0})
+
+    const [ searchCharacters, { loading, error, data }] = useLazyQuery(GET_CHARACTERS(page, searchedString))
 
     useEffect(() => {
         if (!searchedString) return;
         if (searchedString) searchCharacters();
         if (data) {
             setResults(data.characters.results);
+            setPageInfo(data.characters.info);
         }
-    }, [searchedString, data, searchCharacters])
+    }, [searchedString, data, searchCharacters, page])
 
     useEffect(() => {
         if (!searchedString) {
@@ -56,14 +69,22 @@ function HomePage(props: any) {
         }
         if (allChars) {
             setResults(allChars.characters.results);
+            setPageInfo(allChars.characters.info);
         }
-    }, [allChars, loadCharacters, searchedString])
+        console.log({page});
+    }, [allChars, loadCharacters, searchedString, page])
 
     if (loading || loadingAllChars) return <div>Loading....</div>
     if (error || AllCharsError) return <div>Error...</div>
     // if (!allChars) return <div>No data found!</div>
 
     // if (allChars && allChars?.characters?.results) finalResult = allChars.characters.results;
+
+    // TODO: Handle pagination when user come back from character detail page
+    const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+        setPage(value);
+    }
+    
 
     return(        
         <div>
@@ -88,6 +109,17 @@ function HomePage(props: any) {
                     ))}
                 </Grid>
             </Grid>
+
+            {pageInfo && 
+                <Pagination 
+                    defaultPage={page}
+                    count={pageInfo['pages']}
+                    variant="outlined"
+                    shape="rounded"
+                    onChange={handlePageChange}
+                    className={classes.pagination}
+                ></Pagination>
+            }
            
             
         </div>
